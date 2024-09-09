65e67e9d277720137711d8a2ee5b0ad2fe2958d9
kinardes <alicakinar@gmail.com>
2024-09-09 17:46:46 +0700

    Memulai Tugas 2

---
+++ alicious_florist/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for alicious_florist project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alicious_florist.settings')
+
+application = get_wsgi_application()
