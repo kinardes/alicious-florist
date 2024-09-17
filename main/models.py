4ad283f2b07322a980da8f80b4e4bb2890fcd42a
kinardes <alicakinar@gmail.com>
2024-09-17 23:19:37 +0700

    Tugas 3 Done

--- main/models.py
+++ main/models.py
@@ -1,6 +1,8 @@
+import uuid  # tambahkan baris ini di paling atas
 from django.db import models
 
 class Shop(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)  # tambahkan baris ini
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
